1822ef1e3873adfc5199b1c1c70ce53f9b9b493b
Onur Kayahan <onurkayahan@upoint.io>
2024-05-17 16:54:39 +0300

    - ball count reverted back to 10

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -27,7 +27,7 @@ const selectedNumber = ref<number>(0);
 const isCurrentRoundWin = ref<boolean>(false);
 const balance = ref<number>(3);
 
-const items = [1, 2];
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const pool: Array<number> = [];
 
